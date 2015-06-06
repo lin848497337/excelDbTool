7f3028e880e0f7f1fe9e3682c01ca81791a54ac1
lyf <848497337@qq.com>
2015-06-06 14:17:41 +0800

    增加转移旧版本数据功能

--- src/xls/meta/ExcelCreater.java
+++ src/xls/meta/ExcelCreater.java
@@ -2,23 +2,23 @@ package xls.meta;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataValidation;
-import org.apache.poi.ss.usermodel.DataValidationConstraint;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.ss.util.CellRangeAddressList;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelCreater {
 	
 	private TableMetaData table;
+	private Map<Integer,Map<String,Object>> dataMap = new HashMap<Integer,Map<String,Object>>();
 	
 	public ExcelCreater(TableMetaData table){
 		this.table = table;
@@ -29,9 +29,10 @@ public class ExcelCreater {
 		if(!dirfile.exists()){
 			dirfile.mkdir();
 		}
+		int oldRow = 0;
 		File file = new File(outputDir + File.separator + table.excelFile);
 		if(file.exists()){
-			loadData(file);
+			oldRow = loadData(file);
 			file.delete();
 			file = new File(outputDir + File.separator + table.excelFile);
 		}
@@ -58,14 +59,48 @@ public class ExcelCreater {
 			DataValidation dataValidation = col.typeObject.createDataValidation(col, sheet, maxRowNum, index);
 			sheet.addValidationData(dataValidation);
 		}
+		for(int i=1 ; i<=oldRow ; i++){
+			row = sheet.createRow(roleIndex++);
+			index = 0;
+			for(ColMetaData col : table.colList){
+				Cell cell = row.createCell(index++);
+				Map<String,Object> mp = dataMap.get(i);
+				Object val = mp.get(col.colName);
+				if(col.typeObject instanceof IntegerType){
+					cell.setCellValue(Integer.parseInt(val.toString()));
+				}else{
+					cell.setCellValue(val.toString());
+				}
+			}
+		}
 		FileOutputStream fos = new FileOutputStream(file);
 		workbook.write(fos);
 		fos.close();
 		workbook.close();
 	}
 	
-	private void loadData(File file ){
-		
+	private int loadData(File file ) throws Exception{
+		Workbook workbook = WorkbookFactory.create(file);
+		Sheet sheet = workbook.getSheet("data");
+		int rowNum = sheet.getLastRowNum();
+		for(int i=1 ; i<=rowNum ; i++){
+			Row row = sheet.getRow(i);
+			int colNum = row.getLastCellNum();
+			Map<String,Object> mp = new HashMap<String, Object>();
+			dataMap.put(i, mp);
+			for(int j=0 ; j<colNum ; j++){
+				Cell cell = row.getCell(j);
+				ColMetaData colMeta = table.colList.get(j); 
+				if(colMeta.typeObject instanceof StringType){
+					mp.put(colMeta.colName, cell.getStringCellValue());
+				}else if(colMeta.typeObject instanceof EnumMetaData){
+					mp.put(colMeta.colName, cell.getStringCellValue());
+				}else if(colMeta.typeObject instanceof IntegerType){
+					mp.put(colMeta.colName, (int)cell.getNumericCellValue());
+				}
+			}
+		}
+		return rowNum;
 	}
 	
 }
