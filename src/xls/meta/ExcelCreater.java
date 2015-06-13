b0e00b32db0ab38a0f3ae38d30e63ae0f19526a4
lyf <848497337@qq.com>
2015-06-13 23:13:46 +0800

    解决无法复制就excel数据到新生成excelbug

--- src/xls/meta/ExcelCreater.java
+++ src/xls/meta/ExcelCreater.java
@@ -61,17 +61,19 @@ public class ExcelCreater {
 		for(int i=1 ; i<=oldRow ; i++){
 			row = sheet.createRow(roleIndex++);
 			index = 0;
-			ColMetaData col = table.colList.get(i);
-			Cell cell = row.createCell(index++);
-			Map<String,Object> mp = dataMap.get(i);
-			Object val = mp.get(col.colName);
-			if(val == null){
-				continue;
-			}
-			if(col.typeObject instanceof IntegerType){
-				cell.setCellValue(Integer.parseInt(val.toString()));
-			}else{
-				cell.setCellValue(val.toString());
+			for(int j=0 ; j<table.colList.size() ; j++){
+				ColMetaData col = table.colList.get(j);
+				Cell cell = row.createCell(index++);
+				Map<String,Object> mp = dataMap.get(i);
+				Object val = mp.get(col.colName);
+				if(val == null){
+					continue;
+				}
+				if(col.typeObject instanceof IntegerType){
+					cell.setCellValue(Integer.parseInt(val.toString()));
+				}else{
+					cell.setCellValue(val.toString());
+				}
 			}
 		}
 		FileOutputStream fos = new FileOutputStream(file);
